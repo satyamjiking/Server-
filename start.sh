6525b26ef0ae7a7f22fb50352c28368a07ba2447
satyamjiking <116352076+satyamjiking@users.noreply.github.com>
2025-09-28 15:58:26 +0530

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,3 +1,3 @@
 #!/usr/bin/env bash
-python3 main.py &
-gunicorn main:app --bind 0.0.0.0:$PORT
+# ensure executable (git + render) and run gunicorn
+gunicorn main:app --bind 0.0.0.0:$PORT --workers 1 --threads 2
